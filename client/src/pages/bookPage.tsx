00c3d046b7032bf5e65de1ab5beaf3d43b1fcefa
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-09-17 21:43:23 -0600

    added bookpage.tsx and finsihed userpage.tsx

---
+++ client/src/pages/bookPage.tsx
@@ -0,0 +1,24 @@
+export function BookPage() {
+    return (
+        <div> 
+            <aside> 
+                <section>
+                   <img />   {/* image of cover */}
+                </section>
+            </aside>
+            <aside>
+                <section>
+                    <h2>Book Info:</h2>
+                    <h2>Title:</h2>
+                    <h2>Author:</h2>
+                    <h2>Genre:</h2>
+                    <h2>Synopsis:</h2>
+                    <p></p>
+                    <h2>Publication date:</h2>
+                    <button type="button">Add to BookShelf</button>
+                    <button type="button">Add to Bookmarks</button>
+                </section>
+            </aside>
+        </div>
+    )
+}
---
+++ client/src/pages/userpage.tsx
@@ -0,0 +1,20 @@
+export function UserPage() {
+    return (
+        <div> 
+            <aside>
+                <section>
+                    <button type="submit">My Bookshelf</button>
+                    <ul>
+
+                    </ul>
+                </section>
+                <section>
+                    <button type="submit">Bookmarks</button>
+                    <ul>
+
+                    </ul>
+                </section>
+            </aside>
+        </div>
+    ) 
+}
