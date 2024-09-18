0209fce4929b1bd7773a7f0ec6c495d72a7fe880
Ian Ferguson <iansterlingferguson@gmail.com>
2024-09-17 20:26:08 -0700

    basic formatting for navbar, footer, and home pages.

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import auth from '../utils/auth';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import HorizontalLogo from "../assets/baseLogoHorizontal.png";
+import auth from "../utils/auth";
 
 const Navbar = () => {
-  const [ loginCheck, setLoginCheck ] = useState(false);
+  const [loginCheck, setLoginCheck] = useState(false);
 
   const checkLogin = () => {
-    if(auth.loggedIn()) {
+    if (auth.loggedIn()) {
       setLoginCheck(true);
     }
   };
@@ -14,33 +15,57 @@ const Navbar = () => {
   useEffect(() => {
     console.log(loginCheck);
     checkLogin();
-  }, [loginCheck])
+  }, [loginCheck]);
 
   return (
-    <div className='nav'>
-      <div className='nav-title'>
-        <Link to='/'>ReadIt</Link>
+    <div className="navbar navbar-expand-lg mb-4">
+      <div className="container-fluid">
+        <img
+          className="navbar-brand"
+          src={HorizontalLogo}
+          alt="ReadIt Logo"
+          style={{ maxHeight: "145px" }}
+        />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainMenu"
+          aria-controls="mainMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="mainMenu">
+          <ul className="navbar-nav ms-auto mb-2 mb-xl-0">
+            {!loginCheck ? (
+              <li className="nav-item">
+                <Link to="/login">
+                  <button type="button" className="btn btn-primary">
+                    Login
+                  </button>
+                </Link>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => {
+                    auth.logout();
+                  }}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
-      <ul>
-      {
-        !loginCheck ? (
-          <li className='nav-item'>
-            <button type='button'>
-              <Link to='/login'>Login</Link>
-            </button>
-          </li>
-        ) : (
-          <li className='nav-item'>
-            <button type='button' onClick={() => {
-              auth.logout();
-            }}>Logout</button>
-          </li>
-        )
-      }
-      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
 
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,9 +1,30 @@
-import header from '../pages/header.tsx';
-import footer from '../pages/footer.tsx';
-//home page
+import Logo from "../assets/logoOnly.png";
 
 export default function Home() {
-    return (
-        
-    )
+  return (
+    <main className="container">
+      <div className="row">
+        <aside className="col-md-5">
+          <section>
+            <h2>New York Times Best Sellers:</h2>
+          </section>
+        </aside>
+
+        <section className="col-md-7 d-flex flex-column align-items-center text-center">
+          <h2>Find, Organize, and Never Lose Track of a Book Again!</h2>
+          <img src={Logo} alt="logo" className="my-3" />
+          <div className="input-group my-2" style={{ maxWidth: "400px" }}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Start your journey here..."
+            />
+            <button className="btn btn-primary" type="button">
+              Search
+            </button>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
