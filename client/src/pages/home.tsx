e63195c4d7d09dd76c56a0f9ae952e0fe9fb40ae
Ian Ferguson <iansterlingferguson@gmail.com>
2024-09-22 20:14:31 -0700

    added Kagen's files.

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,41 +1,19 @@
 import Logo from "../assets/logoOnly.png";
-// import BestSellerCards from "../components/bestSellerCard.tsx";
-
-// const bestSellers = [
-//   {
-//     rank: 1,
-//     image:
-//       "https://storage.googleapis.com/du-prd/books/images/9781250289568.jpg",
-//     title: "Passions In Death",
-//     author: "J.D. Robb",
-//   },
-// ];
+import { SearchBar } from "../components/SearchBar";
+import { SearchResultsList } from "../components/SearchResultsList";
+import { useState } from "react";
 
 export default function Home() {
+  // Define state to hold search results
+  const [results, setResults] = useState<string[]>([]);
+
   return (
     <main className="container">
       <div className="row">
-        {/* <aside className="col-md-5 col-md-7">
-          <section>
-            <h2>New York Times Best Sellers:</h2>
-            <div className="d-flex flex-wrap justify-content-around">
-              {bestSellers.map((bestSellers, index) => (
-                <BestSellerCards
-                  key={index}
-                  rank={bestSellers.rank}
-                  image={bestSellers.image}
-                  title={bestSellers.title}
-                  author={bestSellers.author}
-                />
-              ))}
-            </div>
-          </section>
-        </aside> */}
-
         <section className="d-flex flex-column align-items-center text-center">
           <h2>Find, Organize, and Never Lose Track of a Book Again!</h2>
           <img src={Logo} alt="logo" className="my-3" />
-          <div className="input-group my-2" style={{ maxWidth: "400px" }}>
+          {/* <div className="input-group my-2" style={{ maxWidth: "400px" }}>
             <input
               type="text"
               className="form-control"
@@ -44,7 +22,11 @@ export default function Home() {
             <button className="btn btn-primary" type="button">
               Search
             </button>
-          </div>
+          </div> */}
+          {/* Pass setResults to SearchBar */}
+          <SearchBar setResults={setResults} />
+          {/* Pass the results to SearchResultsList */}
+          <SearchResultsList searchResults={results} />
         </section>
       </div>
     </main>
---
+++ server/src/routes/api/google-book-routes.ts
@@ -0,0 +1,176 @@
+import express, { Request, Response, query } from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const router = express.Router();
+const GOOGLE_BOOKS_API_KEY = process.env.GOOGLE_BOOKS_API_KEY;
+const GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/auth/books";
+
+// API route to search for books
+
+router.get("/search", async (req: Request, res: Response) => {
+  try {
+    const { query } = req.query;
+
+    const response = await axios.get(`${GOOGLE_BOOKS_API_URL}/v1/volumes`, {
+      params: {
+        q: query,
+        key: GOOGLE_BOOKS_API_KEY,
+      },
+    });
+
+    res.json(response.data.items);
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/book-covers", async (_req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${GOOGLE_BOOKS_API_URL}/v1/volumes`, {
+      params: {
+        q: query,
+        key: GOOGLE_BOOKS_API_KEY,
+      },
+    });
+
+    const bookCovers = response.data.items.map(
+      (item: any) => item.volumeInfo.imageLinks.thumbnail
+    );
+
+    res.json(bookCovers);
+  } catch (error) {
+    console.error("Error fetching book covers:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//api rout to get book details
+router.get("/book-details", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.query;
+
+    const response = await axios.get(
+      `${GOOGLE_BOOKS_API_URL}/v1/volumes/${id}`,
+      {
+        params: { key: GOOGLE_BOOKS_API_KEY },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching book details:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//api route to get book reviews
+router.get("/book-reviews", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.query;
+
+    const response = await axios.get(
+      `${GOOGLE_BOOKS_API_URL}/v1/volumes/${id}/reviews`,
+      {
+        params: { key: GOOGLE_BOOKS_API_KEY },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching book reviews:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+//api route to get book categories
+router.get("/book-categories", async (_req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${GOOGLE_BOOKS_API_URL}/v1/volumes`, {
+      params: { key: GOOGLE_BOOKS_API_KEY },
+    });
+
+    const categories = response.data.items.map(
+      (item: any) => item.volumeInfo.categories
+    );
+
+    res.json(categories);
+  } catch (error) {
+    console.error("Error fetching book categories:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+//api to get book authors
+router.get("/book-authors", async (_req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${GOOGLE_BOOKS_API_URL}/v1/volumes`, {
+      params: { key: GOOGLE_BOOKS_API_KEY },
+    });
+
+    const authors = response.data.items.map(
+      (item: any) => item.volumeInfo.authors
+    );
+
+    res.json(authors);
+  } catch (error) {
+    console.error("Error fetching book authors:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+//api to get book ratings
+router.get("/book-ratings", async (_req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${GOOGLE_BOOKS_API_URL}/v1/volumes`, {
+      params: { key: GOOGLE_BOOKS_API_KEY },
+    });
+
+    const ratings = response.data.items.map(
+      (item: any) => item.volumeInfo.averageRating
+    );
+
+    res.json(ratings);
+  } catch (error) {
+    console.error("Error fetching book ratings:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+//api for filterign books
+router.get("/filter", async (req: Request, res: Response) => {
+  try {
+    const { query } = req.query;
+
+    const response = await axios.get(`${GOOGLE_BOOKS_API_URL}/v1/volumes`, {
+      params: {
+        q: query,
+        key: GOOGLE_BOOKS_API_KEY,
+      },
+    });
+
+    res.json(response.data.items);
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//api to get book titles
+router.get("/book-titles", async (_req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${GOOGLE_BOOKS_API_URL}/v1/volumes`, {
+      params: { key: GOOGLE_BOOKS_API_KEY },
+    });
+
+    const titles = response.data.items.map(
+      (item: any) => item.volumeInfo.title
+    );
+
+    res.json(titles);
+  } catch (error) {
+    console.error("Error fetching book titles:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+export default router;
